bf40a6bf270d0032f0416bbd4be9cb7e66399ba5
Richard Remer <rich.remer@gmail.com>
2022-03-14 10:11:14 -0700

    better test script

--- tool/test-redis-socket.php
+++ tool/test-redis-socket.php
@@ -2,10 +2,16 @@
 
 \$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
-socket_connect(\$socket, "redis", 6379);
-socket_write(\$socket, "SET testkey testval\n");
-
-while (true) {
-  echo socket_read(\$socket, 1024, PHP_NORMAL_READ);
-  sleep(1);
+if (!is_resource(\$socket)) {
+  fprintf(STDERR, "failed to create socket\n");
+  exit(1);
 }
+
+if (!socket_connect(\$socket, "redis", 6379)) {
+  fprintf(STDERR, "could not connect to redis:6379\n");
+  exit(1);
+}
+
+socket_write(\$socket, "SET testkey testval\n");
+sleep(1);
+echo socket_read(\$socket, 1024, PHP_NORMAL_READ);
