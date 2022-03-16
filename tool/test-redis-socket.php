d4c76bdd8e6fdb93e2733d1e7fd887add3e2163f
Richard Remer <rich.remer@gmail.com>
2022-03-16 11:26:18 -0700

    update test script

--- tool/test-redis-socket.php
+++ tool/test-redis-socket.php
@@ -12,6 +12,17 @@ if (!socket_connect(\$socket, "redis", 6379)) {
   exit(1);
 }
 
-socket_write(\$socket, "SET testkey testval\n");
-sleep(1);
-echo socket_read(\$socket, 1024, PHP_NORMAL_READ);
+switch (\$argv[1]) {
+  case "read":
+    socket_write(\$socket, "GET testkey\n");
+    sleep(0.5);
+    echo socket_read(\$socket, 1024, PHP_NORMAL_READ);
+    break;
+  case "write":
+    socket_write(\$socket, "SET testkey scriptval\n");
+    sleep(0.5);
+    echo socket_read(\$socket, 1024, PHP_NORMAL_READ);
+    break;
+  default:
+    echo "did nothing\n";
+}
